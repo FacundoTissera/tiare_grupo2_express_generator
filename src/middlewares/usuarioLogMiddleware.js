3fdc24720a2986f315cdafcd2c3fb41a154e8e29
bhambeck <bhambeck@gmail.com>
2021-10-05 12:29:48 -0300

    user user

--- src/middlewares/usuarioLogMiddleware.js
+++ src/middlewares/usuarioLogMiddleware.js
@@ -1,25 +1,23 @@
 const db = require('../database/models');
-const User = require('../models/User');
 
-
-function usuarioLogMiddleware (req, res, next){
+async function usuarioLogMiddleware (req, res, next){
 
     res.locals.isLogged = false;
 
-    db.Usuario.findOne({
-        where: {
-            email: req.cookies.userEmail
-        }
-    })
-    .then((userFromCookie) => {
+    if (req.cookies.userEmail) {
+        let userFromCookie = await db.Usuario.findOne({
+            where: {
+                email: req.cookies.userEmail
+            }
+        });
         if (userFromCookie) {
             req.session.usuarioLogueado = userFromCookie; 
         }
         if (req.session && req.session.usuarioLogueado){
             res.locals.isLogged = true;
-        }
-    }).finally(() => {
-        next();
-    });
+        }    
+    }
+    
+    next();
 };
 module.exports = usuarioLogMiddleware;
