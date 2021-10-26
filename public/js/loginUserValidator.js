0689ff5f9a0bdce498c40757ee52a5c65a130752
Facundo <facundotisserasorribas@gmail.com>
2021-10-26 19:37:55 -0300

    Validaciones del lado del front para registro de usuarios terminado

---
+++ public/js/loginUserValidator.js
@@ -0,0 +1,18 @@
+window.addEventListener('load',function(){
+    //alert('Login gatyo')
+    const formularioLogin = document.querySelector('#formularioLogin');
+    const inputs = document.querySelectorAll('formularioLogin input')
+    
+    const expresiones = {
+        email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+        password: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    }
+
+    const camposForm = {
+        
+    }
+
+    const validarFormularioLogin = (e) =>{
+
+    }
+})
