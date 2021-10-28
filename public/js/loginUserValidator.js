657c312a97de47fc6631661a74b06062802f84a0
Facundo <facundotisserasorribas@gmail.com>
2021-10-28 19:37:05 -0300

    validaciones lado del front para Login terminado

--- public/js/loginUserValidator.js
+++ public/js/loginUserValidator.js
@@ -1,18 +1,124 @@
 window.addEventListener('load',function(){
-    //alert('Login gatyo')
-    const formularioLogin = document.querySelector('#formularioLogin');
-    const inputs = document.querySelectorAll('formularioLogin input')
     
+    const formularioLogin = document.querySelector('#formularioLogin');
+    const inputs = document.querySelectorAll('#formularioLogin input')
+    //console.log(inputs);
     const expresiones = {
         email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-        password: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-    }
-
-    const camposForm = {
+        password: /^.{8,12}$/,
+    };
+    const validarFormulario = (e)=>{
+        //console.log(e.target.name); 
+        switch (e.target.name) {
+            case 'email':
+                if (expresiones.email.test(e.target.value)) {
+                    document.getElementById('grupo__email').classList.remove('formulario_grupo-incorrecto');
+                    document.getElementById('grupo__email').classList.add('formulario_grupo-correcto');
+                    document.querySelector('#grupo__email i').classList.remove('fa-times-circle');
+                    document.querySelector('#grupo__email i').classList.add('fa-check-circle');
+                    document.querySelector('#grupo__email .formulario_input-error').classList.remove('formulario_input-error-activo');
+                }else{
+                    document.getElementById('grupo__email').classList.add('formulario_grupo-incorrecto');
+                    document.getElementById('grupo__email').classList.remove('formulario_grupo-correcto');
+                    document.querySelector('#grupo__email i').classList.add('fa-times-circle');
+                    document.querySelector('#grupo__email i').classList.remove('fa-check-circle');
+                    document.querySelector('#grupo__email .formulario_input-error').classList.add('formulario_input-error-activo')
+                }
+                break;
+            case 'password':
+                if (expresiones.password.test(e.target.value)) {
+                    document.getElementById('grupo__password').classList.remove('formulario_grupo-incorrecto');
+                    document.getElementById('grupo__password').classList.add('formulario_grupo-correcto');
+                    document.querySelector('#grupo__password i').classList.remove('fa-times-circle');
+                    document.querySelector('#grupo__password i').classList.add('fa-check-circle');
+                    document.querySelector('#grupo__password .formulario_input-error').classList.remove('formulario_input-error-activo');
+                }else{
+                    document.getElementById('grupo__password').classList.add('formulario_grupo-incorrecto');
+                    document.getElementById('grupo__password').classList.remove('formulario_grupo-correcto');
+                    document.querySelector('#grupo__password i').classList.add('fa-times-circle');
+                    document.querySelector('#grupo__password i').classList.remove('fa-check-circle');
+                    document.querySelector('#grupo__password .formulario_input-error').classList.add('formulario_input-error-activo')
+                }
         
+            default:
+                break;
+        } 
     }
+    inputs.forEach((input)=>{
+        input.addEventListener('keyup', validarFormulario);
+        input.addEventListener('blur', validarFormulario);
+        })
+    
 
-    const validarFormularioLogin = (e) =>{
+    formularioLogin.addEventListener("submit",(e)=>{
+        formularioLogin.submit();
+    })
 
-    }
 })
+// llamo a los id del div de cada elemento
+
+// })   const camposForm = {
+//         emailUsuario:false,
+//         contraseniaUsuario:false
+//     }
+
+//     const validarFormularioLogin = (e) =>{
+//         //identifico los name de el login console.log(e.target.name);
+//         switch (e.target.name) {
+//             case 'email':
+//                 validarCampo(expresiones.email, e.target, "emailUsuario");
+//                 break;
+        
+//             case 'password':
+//                 validarCampo(expresiones.password, e.target, "contraseniaUsuario");
+//                 break;
+        
+//             default:
+//                 break;
+//         }
+//     };
+
+//     const validarCampo = (expresion, input, campo) =>{
+//         if (expresion.test(input.value)) {
+//                 document
+//                 .getElementById(`${campo}`)
+//                 .classList.remove("formulario_grupo-incorrecto");
+//             document
+//                 .getElementById(`${campo}`)
+//                 .classList.add("formulario_grupo-correcto");
+//             document.querySelector(`#${campo} i`).classList.add("fa-check-circle");
+//             document.querySelector(`#${campo} i`).classList.remove("fa-times-circle");
+//             document
+//                 .querySelector(`#${campo} .formulario_input-error`)
+//                 .classList.remove("formulario_input-error-activo");
+//             camposForm[campo] = true;
+//             } else {
+//                 document
+//             .getElementById(`${campo}`)
+//             .classList.add("formulario_grupo-incorrecto");
+//         document
+//             .getElementById(`${campo}`)
+//             .classList.remove("formulario_grupo-correcto");
+//         document.querySelector(`#${campo} i`).classList.remove("fa-check-circle");
+//         document.querySelector(`#${campo} i`).classList.add("fa-times-circle");
+//         document
+//             .querySelector(`#${campo} .formulario_input-error`)
+//             .classList.add("formulario_input-error-activo");
+//         camposForm[campo] = false;
+//         }
+//     };
+//     inputs.forEach((input) => {
+//             input.addEventListener("keyup", validarFormularioLogin);
+//             input.addEventListener("blur", validarFormularioLogin);
+        
+//     });
+//     formularioLogin.addEventListener('submit', (e)=>{
+//         e.preventDefault();
+//         if (camposForm.emailUsuario && camposForm.contraseniaUsuario) {
+//             formularioLogin.submit();
+//         } else {
+//             document
+//             .getElementById("formulario__mensaje")
+//             .classList.add("formulario__mensaje-activo");
+//         }
+//     })
--- public/js/userValidatorFront.js
+++ public/js/userValidatorFront.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function () {
   const expresiones = {
     //usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
     nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-    password: /^.{4,12}$/, // 4 a 12 digitos.
+    password: /^.{8,12}$/, // 4 a 12 digitos.
     email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
     numeros: /^\d{7,14}$/, // 7 a 14 numeros.
     codigoPostal: /^\d{0,4}$/,
