8b4cda9fb0c9343b7e177b875164713413ef7a59
bhambeck <bhambeck@gmail.com>
2021-11-02 14:36:20 -0300

    api productos lista

--- src/controllers/api/productApiController.js
+++ src/controllers/api/productApiController.js
@@ -1,26 +1,61 @@
 const db = require('../../database/models');
+const productsController = require('../productsController');
+const Op = db.Sequelize.Op
 
 const productApiController = {
     'list': (req, res) => {
         db.Producto
-            .findAll()
+            .findAll({
+                include: ['categorias']
+            })
             .then(productos => {
+                //console.log (productos[8].categorias.category)
+                let prodCateg = {};
+                for (let i = 0; i <productos.length; i++) {
+                    let producto = productos[i];
+                    let categoria = producto.categorias.category;
+                    if (prodCateg[categoria]) {
+                        prodCateg[categoria] = prodCateg[categoria] + 1;
+                    } 
+                    else {
+                        prodCateg[categoria] = 1;
+                    }
+                }
+                let datosProducto =[]
+                for (let i=0; i <productos.length; i++){
+                    let unProducto = {
+                       id:productos[i].id, 
+                       name:productos[i].name, 
+                       description:productos[i].description, 
+                       category:productos[i].categorias.category, 
+                       detail:'api/products/'+productos[i].id
+                    }
+                datosProducto.push(unProducto)  
+                }
                 return res.status(200).json({
+                    
                     meta:{
                         status:200, 
                         count: productos.length,
+                        countByCategory: prodCateg,
                         url:'api/products/'
                     },
-                    data:productos
                     
+                    data:{
+                      
+                        datosProducto, 
+                    }
                 })
-            })
-    // },
-    // 'detail': (req, res) => {
-    //     db.Genre.findByPk(req.params.id)
-    //         .then(genre => {
-    //             res.render('genresDetail.ejs', {genre});
-    //         });
+            }
+        )},
+    'detail': (req, res) => {
+        db.Producto
+            .findByPk(req.params.id)
+            .then(producto => {
+                return res.status(200).json({
+                    data:producto
+                });
+            });
     }
 
 }
--- src/routes/api/productApiRouter.js
+++ src/routes/api/productApiRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const productApiController = require('../../controllers/api/productApiController');
 
 router.get('/', productApiController.list);
-// router.get('/:id', productApiController.detail);
+router.get('/:id', productApiController.detail);
 
 
 module.exports = router;
