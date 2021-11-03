4cc27281f63c326c860891066affe1450abac31f
bhambeck <bhambeck@gmail.com>
2021-11-03 15:25:08 -0300

    api detalle producto

--- src/controllers/api/productApiController.js
+++ src/controllers/api/productApiController.js
@@ -3,13 +3,14 @@ const productsController = require('../productsController');
 const Op = db.Sequelize.Op
 
 const productApiController = {
+    //listado de todos los productos
     'list': (req, res) => {
         db.Producto
             .findAll({
                 include: ['categorias']
             })
             .then(productos => {
-                //console.log (productos[8].categorias.category)
+                //console.log (productos[8].categorias.category) Para encontrar categoria
                 let prodCateg = {};
                 for (let i = 0; i <productos.length; i++) {
                     let producto = productos[i];
@@ -21,7 +22,7 @@ const productApiController = {
                         prodCateg[categoria] = 1;
                     }
                 }
-                let datosProducto =[]
+                let products =[]
                 for (let i=0; i <productos.length; i++){
                     let unProducto = {
                        id:productos[i].id, 
@@ -30,7 +31,7 @@ const productApiController = {
                        category:productos[i].categorias.category, 
                        detail:'api/products/'+productos[i].id
                     }
-                datosProducto.push(unProducto)  
+                products.push(unProducto)  
                 }
                 return res.status(200).json({
                     
@@ -42,18 +43,43 @@ const productApiController = {
                     },
                     
                     data:{
-                      
-                        datosProducto, 
+                        products, 
                     }
                 })
             }
         )},
     'detail': (req, res) => {
         db.Producto
-            .findByPk(req.params.id)
+            .findByPk(req.params.id,{
+                include:[{association:"categorias"}, {association:"talles"}, {
+                    association:"colores"}
+                ]
+            })
             .then(producto => {
+                //console.log(producto.talles[0].size)
+                let arrayTalles = []
+                for (let i=0; i<producto.talles.length;i++){
+                    let unTalle=producto.talles[i].size
+                    arrayTalles.push(unTalle)
+                     
+                 }
+                 let arrayColores = []
+                for (let i=0; i<producto.colores.length;i++){
+                    let unColor=producto.colores[i].color
+                    arrayColores.push(unColor)
+                }
+
+                let objeto = {
+                    id:producto.id, 
+                    name:producto.name, 
+                    description:producto.description, 
+                    category:producto.categorias.category, 
+                    sizes: arrayTalles,
+                    colors:arrayColores                
+                 }
                 return res.status(200).json({
-                    data:producto
+                    
+                    data:objeto
                 });
             });
     }
